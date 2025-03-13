3bacdf7a3cf7261dada01e70d7abaa47d46c838e
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2025-03-13 16:57:50 -0300

    add: gmail toolkit

--- modules/langgraph/tools/tools.py
+++ modules/langgraph/tools/tools.py
@@ -10,6 +10,7 @@ import os
 tools = [upsert_memory_tool, current_screen_tool, resume_tts_tool, stop_tts_tool]
 
 # Only import if plugin is activated
+
 if(os.environ['JIRA_ACTIVATE_PLUGIN'] == "true"):
     from modules.langgraph.tools.jira_toolkit import jira_tools
     tools.extend(jira_tools)
@@ -21,3 +22,7 @@ if(os.environ['OPENRECALL_ACTIVATE_PLUGIN'] == 'true'):
 if(os.environ['BRAVE_SEARCH_ACTIVATE_PLUGIN'] == 'true'):
     from modules.langgraph.tools.brave_search import search_brave_tool
     tools.append(search_brave_tool)
+
+if(os.environ['GMAIL_ACTIVATE_PLUGIN'] == "true"):
+    from modules.langgraph.tools.gmail_toolkit import gmail_tools
+    tools.extend(gmail_tools)
